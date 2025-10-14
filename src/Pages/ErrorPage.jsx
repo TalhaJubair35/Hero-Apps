c16f704bb6095a725c82b1e7cf9d2f3aff0f074d
TalhaJubair35 <jubairtalha15@gmail.com>
2025-10-14 15:56:59 +0600

    first commit

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+const ErrorPage = () => {
+  
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>error</div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default ErrorPage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../Pages/Home";
+import Apps from "../Pages/Apps";
+import MainLayout from "../Layout/MainLayout";
+import Installation from "../Pages/Installation";
+import ErrorPage from "../Pages/ErrorPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        index:true,
+        Component: Home,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/installation",
+        Component:Installation ,
+      },
+    ],
+  },
+]);
+export default router;
