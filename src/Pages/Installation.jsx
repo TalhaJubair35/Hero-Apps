0f7ce911db3d2274512ef22b8cf45a2278e63c6f
TalhaJubair35 <jubairtalha15@gmail.com>
2025-10-16 02:34:58 +0600

    done

--- src/Pages/Installation.jsx
+++ src/Pages/Installation.jsx
@@ -33,7 +33,6 @@ const Installation = () => {
       <h2 className="text-4xl font-bold text-center text-[#392f5a] mb-10">
         Installed Apps
       </h2>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {installedApps.map((app) => (
           <div
